c13f86016c934325c3f7d64d826fe5de76def63f
Jdilsham <janithadilsham@gmail.com>
2025-12-03 20:25:33 +0530

    add

--- lesson-13/locals/main.tf
+++ lesson-13/locals/main.tf
@@ -13,18 +13,5 @@ provider "aws" {
   region = "us-east-2"
 }
 
-## Build an IAM user account resource here with a for_each meta-argument referecing a locals block.
-
-
-
-## Then build a locals block with a toset function listing 4 users.
-
-
-
-## Finally, build a locals block with common tags and reference it from the IAM user resource.
-
-
-
-## More information on Locals: https://developer.hashicorp.com/terraform/language/values/locals
 
 
